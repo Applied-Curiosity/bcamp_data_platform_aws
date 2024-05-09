50a9d2bed6e4abd008079ff8e1468b30526f1f19
Shawn Deggans @ Applied Curiosity <153559176+shawndeggans-appliedc@users.noreply.github.com>
2024-05-09 18:33:57 +0000

    Checking in changes to test the action

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,35 +1,17 @@
-import json
+"""An AWS Python Pulumi program"""
+
 import pulumi
-import pulumi_aws as aws
-import pulumi_aws_apigateway as apigateway
+from pulumi_aws_native import s3
 
-# An execution role to use for the Lambda function
-role = aws.iam.Role("role", 
-    assume_role_policy=json.dumps({
-        "Version": "2012-10-17",
-        "Statement": [{
-            "Action": "sts:AssumeRole",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "lambda.amazonaws.com",
-            },
-        }],
-    }),
-    managed_policy_arns=[aws.iam.ManagedPolicy.AWS_LAMBDA_BASIC_EXECUTION_ROLE])
+# Create an AWS resource (S3 Bucket)
+bucket = s3.Bucket("my-bucket")
 
-# A Lambda function to invoke
-fn = aws.lambda_.Function("fn",
-    runtime="python3.9",
-    handler="handler.handler",
-    role=role.arn,
-    code=pulumi.FileArchive("./function"))
+# Export the name of the bucket
+pulumi.export("bucket_name", bucket.id)
 
-# A REST API to route requests to HTML content and the Lambda function
-api = apigateway.RestAPI("api",
-  routes=[
-    apigateway.RouteArgs(path="/", local_path="www"),
-    apigateway.RouteArgs(path="/date", method=apigateway.Method.GET, event_handler=fn)
-  ])
+# To test out the CICD process, I'm adding a second bucket
+# Create an AWS resource (S3 Bucket)
+bucket2 = s3.Bucket("my-bucket-2")
 
-# The URL at which the REST API will be served.
-pulumi.export("url", api.url)
+# Export the name of the bucket
+pulumi.export("bucket_name", bucket2.id)
