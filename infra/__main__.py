11dfc908ae55deec0ce0d0513631cc82801f4866
Shawn Deggans @ Applied Curiosity <153559176+shawndeggans-appliedc@users.noreply.github.com>
2024-06-12 18:59:06 +0000

    Checked in remaining resources to test with deployment

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,9 +1,18 @@
-#%%
+# __main__.py
 import pulumi
 import yaml
+import json
 from dto import ConfigDTO
+
+# Import resource classes
 from resources.iam import IAMResource
+from resources.storage import S3Resource
 from resources.vpc import VPCResource
+from resources.security import SecurityResource
+from resources.databricks import DatabricksResource
+from resources.privatelink import PrivateLinkResource
+from resources.kms import KMSResource
+# from resources.bastion import BastionResource
 
 print("Starting Pulumi script...")
 
@@ -15,22 +24,46 @@ print(f"Using configuration file: {config_path}")
 with open(config_path, 'r') as file:
     config_data = yaml.safe_load(file)
 
-print("Configuration data loaded:")
-print(config_data)
 
 # Create DTOs from configuration
 config_dto = ConfigDTO.from_dict(config_data)
-print("DTO created from configuration:")
 
-# Instantiate IAMResource with DTO
+# Instantiate resources with DTO
 iam_resource = IAMResource(config_dto.iam)
-print("IAM Resource instantiated.")
+storage_resource = S3Resource(config_dto.storage)
 
-# Instantiate VPC with DTO
+# Creating VPC resource and getting the VPC ID
 vpc_resource = VPCResource(config_dto.vpc)
+vpc_outputs = vpc_resource.output_dto().outputs
+vpc_id = vpc_outputs['vpc_id']
+subnet_id = vpc_outputs['subnet_ids'][0]
 
-# Export the outputs managed by the IAMResource class
-output_dto = iam_resource.output_dto()
-for key, value in output_dto.outputs.items():
-    pulumi.export(key, value)
-print("Pulumi script completed.")
+# Instantiate security resource and pass the VPC ID to the security resource
+security_resource = SecurityResource(config_dto.security, vpc_id)
+security_outputs = security_resource.output_dto().outputs
+sg_id = security_outputs['security_group_ids'][0]
+
+databricks_resource = DatabricksResource(config_dto.databricks)
+
+# Instantiate the PrivateLink resource
+privatelink_config = config_dto.privatelink
+for endpoint in privatelink_config.endpoints:
+    endpoint['vpc_id'] = vpc_id
+    endpoint['subnet_ids'] = [subnet_id]
+    endpoint['security_group_ids'] = [sg_id]
+privatelink_resource = PrivateLinkResource(config_dto.privatelink)
+
+# Instantiate the KMS resource
+kms_resource = KMSResource(config_dto.kms)
+
+# Bastion resource
+# bastion_resource = BastionResource(config_dto.bastion)
+
+# Export any necessary outputs
+pulumi.export('iam_outputs', iam_resource.output_dto().outputs)
+pulumi.export('S3_outputs', storage_resource.output_dto().outputs)
+pulumi.export('VPC_outputs', vpc_outputs)
+pulumi.export('Security_outputs', security_outputs)
+pulumi.export('databricks_outputs', databricks_resource.output_dto().outputs)
+pulumi.export('privatelink_outputs', privatelink_resource.output_dto().outputs)
+pulumi.export('kms_outputs', kms_resource.output_dto().outputs)
